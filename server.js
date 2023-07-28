91f22c4da068ae1ea54e28a9d4aa59d862ce1378
Brayan Bravo Arébalo <brayan.bravo@rakidwam.cl>
2023-07-28 15:42:45 -0400

    Add room socket

--- server.js
+++ server.js
@@ -1,31 +1,46 @@
 import express from "express";
-import  {Server as socketIO } from "socket.io";
+import { Server as socketIO } from "socket.io";
 import http from "node:http"
 import cors from "cors";
-import { assert } from "node:console";
 
 const app = express()
 const server = http.createServer(app)
-const options ={
-  cors:true,
-  origins:["*"],
- }
-const io = new socketIO(server,options)
+const options = {
+  cors: true,
+  origins: ["*"],
+}
+const io = new socketIO(server, options)
+
+
+
 
 io.on('connection', (socket) => {
   console.log('Usuario conectado:', socket.id);
 
-  // Manejar evento para recibir cambios desde el cliente BPMN.io
-  socket.on('bpmn-update', (data) => {
-    // Aquí podrías agregar lógica para validar los cambios y manejar conflictos
-    // Luego, retransmite los cambios a todos los demás clientes conectados.
-    socket.broadcast.emit('bpmn-update', data);
+  socket.on('join-room', (room) => {
+    const roomName = `sala-${room}`
+    socket.join(roomName);
+    console.log(`Cliente unido a la sala ${room}`);
   });
+
+  socket.on('leave-room', (room) => {
+    const roomName = `sala-${room}`;
+    socket.leave(roomName);
+    console.log(`Cliente salió de la sala ${roomName}`);
+  });
+
+  socket.on('bpmn-update', ({ room, xml }) => {
+    const roomName = `sala-${room}`
+    io.to(roomName).emit('bpmn-update', { xml })
+  });
+
 
   socket.on('disconnect', () => {
     console.log('Usuario desconectado:', socket.id);
   });
 });
+
+
 
 
 app.use(cors())
